1075f1a9dfbbc51ea5954d28e523eff34cc52fbf
stromhurst <saleh.samee@gmail.com>
2023-06-03 19:40:47 +0500

    +CURL functionality to tasks and removed dark mode

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,9 +8,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <x-link href="{{ route('tasks.create') }}" class="m-4">Add new task</x-link>
+                    <table class="w-full text-sm text-left text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                         <tr>
                             <th scope="col" class="px-6 py-3">
                                 Task name
@@ -22,19 +23,26 @@
                         </thead>
                         <tbody>
                         @forelse ($tasks as $task)
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            <tr class="bg-white border-b">
+                                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                     {{ $task->name }}
                                 </td>
                                 <td class="px-6 py-4">
-                                    <a href="{{ route('tasks.edit', $task) }}"
-                                       class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <x-link href="{{ route('tasks.edit', $task) }}">Edit</x-link>
+                                    <form method="POST" action="{{ route('tasks.destroy', $task) }}" class="inline-block">
+                                        @csrf
+                                        @method('DELETE')
+                                        <x-danger-button
+                                            type="submit"
+                                            onclick="return confirm('Are you sure?')">Delete
+                                        </x-danger-button>
+                                    </form>
                                 </td>
                             </tr>
                         @empty
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <tr class="bg-white border-b">
                                 <td colspan="2"
-                                    class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                     {{ __('No tasks found') }}
                                 </td>
                             </tr>
